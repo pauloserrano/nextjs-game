9d52da83f4b7dfa995ea3ef482853b74419b89db
Paulo Serrano <pvsserrano@gmail.com>
2023-09-30 14:47:09 -0300

    feat: overworld ui

--- data/icons.ts
+++ data/icons.ts
@@ -4,3 +4,13 @@ export const Star: Icon = {
   name: "Star",
   src: "/assets/icons/star.svg"
 }
+
+export const Cog: Icon = {
+  name: "Cog",
+  src: "/assets/icons/cog.svg"
+}
+
+export const Speech: Icon = {
+  name: "Speech",
+  src: "/assets/icons/speech.svg"
+}
--- layouts/OverworldLayout/OverworldLayout.tsx
+++ layouts/OverworldLayout/OverworldLayout.tsx
@@ -1,48 +1,101 @@
 import Image from "next/image"
-import { Comfortaa } from "next/font/google"
+import { Comfortaa, Volkhov } from "next/font/google"
 import { Layout } from "@/layouts"
 import { useGameContext } from "@/hooks"
 import styles from "./OverworldLayout.module.scss"
 
 import { Demo, Outside } from "@/data"
 import { Icon } from "@/types"
-import { Star } from "@/data/icons"
+import { Star, Cog, Speech } from "@/data/icons"
 
 
-const font = Comfortaa({ subsets: ['latin'] })
+const comfortaa = Comfortaa({ subsets: ['latin'] })
+const volkhov = Volkhov({ subsets: ["latin"], weight: ["400", "700"]})
 
 export function OverworldLayout() {
   const { state, actions } = useGameContext()
 
   return (
-    <Layout className={`${styles.container} ${font.className}`}>
-      <ul className={styles["navigation-container"]}>
-        <OverworldLayout.NavButton 
+    <Layout className={`${styles.container} ${comfortaa.className}`}>
+      <div className={`${styles["info-container"]} ${volkhov.className}`}>
+        <h3>Wimborn</h3>
+        <hr />
+        <p>Afternoon</p>
+      </div>
+      <ul className={styles["actions-container"]}>
+        <OverworldLayout.ActionButton 
           icon={Star}
           label="Notice Board"
           onClick={() => actions.handleMapChange(state.currentMap.name == "Demo" ? Outside : Demo)}
         />
-        <OverworldLayout.NavButton 
+        <OverworldLayout.ActionButton 
           icon={Star}
           label="Home"
         />
-        <OverworldLayout.NavButton 
+        <OverworldLayout.ActionButton 
           icon={Star}
           label="Leave"
         />
+      </ul>
+      <nav className={styles["nav-container"]}>
+        <button>
+          <Image 
+            fill
+            alt="settings"
+            src={Cog.src}
+          /></button>
+        <button>
+          <Image 
+            fill
+            alt="settings"
+            src={Cog.src}
+          /></button>
+        <button>
+          <Image 
+            fill
+            alt="settings"
+            src={Cog.src}
+          /></button>
+        <button>
+          <Image 
+            fill
+            alt="settings"
+            src={Cog.src}
+          />
+        </button>
+      </nav>
+
+      <ul className={styles["party-container"]}>
+        {state.characters.map((character, id) => (
+          <li key={id}>
+            <Image 
+              width={40}
+              height={30}
+              className={styles.speech}
+              alt="chat"
+              src={Speech.src}
+            />
+            <Image 
+              fill
+              className={styles.portrait}
+              alt={character.name}
+              src={character.src}
+            />
+          </li>
+        ))}
       </ul>
     </Layout>
   )
 }
 
 
-interface NavButtonProps { 
+interface ActionButtonProps { 
   label: string, 
   icon: Icon, 
   [prop: string]: any 
 }
 
-OverworldLayout.NavButton = function NavButton({ label, icon, ...props }: NavButtonProps) {
+OverworldLayout.ActionButton = function ActionButton({ label, icon, ...props }: ActionButtonProps) {
   return (
     <li {...props}>
       <div className={styles["icon-container"]}>
